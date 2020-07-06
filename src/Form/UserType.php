7f29b0b31a3699eac5da0ff49a0fe37b49ae323a
jfsenechal <jfsenechal@gmail.com>
2020-07-06 08:49:54 +0200

    user controller

---
+++ src/Form/UserType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AcMarche\Api\Form;
+
+use AcMarche\Api\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('email')
+            ->add('nom')
+            ->add('roles')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
