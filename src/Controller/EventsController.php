56f349017f516075d4e9d600836a4f9eac5c36ac
jfsenechal <jfsenechal@gmail.com>
2024-01-11 09:16:56 +0100

    up events

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -12,8 +12,8 @@ class EventsController extends AbstractController
     #[Route(path: '/evenements', name: 'events')]
     public function index(): JsonResponse
     {
-        $hadesEvent = new HadesEvent();
-        $events = $hadesEvent->getItems();
+        $content_json = file_get_contents("https://www.marche.be/api/actus.php");
+        $events = json_decode($content_json, null, 512, JSON_THROW_ON_ERROR);
 
         return new JsonResponse($events);
     }
