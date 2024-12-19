6d668ed8dd5306507e288694d3d6dd415047533a
jfsenechal <jfsenechal@gmail.com>
2024-12-19 16:29:58 +0100

    test method

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -100,11 +100,10 @@ class DefaultController extends AbstractController
         );
     }
 
-    #[Route(path: '/parking')]
-    public function parkingApi(Request $request): JsonResponse
+    #[Route(path: '/parking', methods: ['POST'])]
+    public function parking(Request $request): JsonResponse
     {
         $jsonString = $request->getContent();
-            $this->apiMailer->sendError('error:'.$request->getMethod());
         try {
             $eventNotification = new EventNotification($jsonString);
             if (!$parking = $this->parkingRepository->findByNumber($eventNotification->data->id)) {
