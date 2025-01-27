aee4de16698fbe2f8cfba6a046741d4689bb1df6
jfsenechal <jfsenechal@gmail.com>
2025-01-27 11:09:47 +0100

    add

--- src/Controller/TokenController.php
+++ src/Controller/TokenController.php
@@ -5,7 +5,6 @@ namespace AcMarche\Api\Controller;
 use AcMarche\Api\Entity\User;
 use AcMarche\Api\Security\TokenGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -21,8 +20,9 @@ class TokenController extends AbstractController
     #[Route(path: '/{id}', name: 'api_token_generate', methods: ['GET'])]
     public function index(User $user): Response
     {
-        $token = $this->tokenGenerator->generateToken($user);
+        $tokenString = $this->tokenGenerator->generateToken($user);
+        $this->addFlash('success', 'Token created '.$tokenString);
 
-        return new JsonResponse(['token' => $token]);
+        return $this->redirectToRoute('api_user_show', ['id' => $user->getId()]);
     }
 }
