9dabe750f3b59645f0c5079a85cbbd4fabb997cf
jfsenechal <jfsenechal@gmail.com>
2025-01-27 09:41:15 +0100

    remove log

--- src/Security/AccessTokenHandler.php
+++ src/Security/AccessTokenHandler.php
@@ -3,7 +3,6 @@
 namespace AcMarche\Api\Security;
 
 use AcMarche\Api\Repository\AccessTokenRepository;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -12,7 +11,6 @@ class AccessTokenHandler implements AccessTokenHandlerInterface
 {
     public function __construct(
         private AccessTokenRepository $accessTokenRepository,
-        private LoggerInterface $logger,
     ) {}
 
     public function getUserBadgeFrom(#[\SensitiveParameter] string $accessToken): UserBadge
@@ -22,7 +20,6 @@ class AccessTokenHandler implements AccessTokenHandlerInterface
         if (null === $token || !$token->isValid()) {
             throw new BadCredentialsException('Invalid token.');
         }
-        $this->logger->debug("zeze ok username ".$token->user->getUserIdentifier());
         // and return a UserBadge object containing the user identifier from the found token
         // (this is the same identifier used in Security configuration; it can be an email,
         // a UUID, a username, a database ID, etc.)
